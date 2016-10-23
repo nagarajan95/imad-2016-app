228db36865f2140e115a496eb7bd762b7ef7807a
nagarajan95 <l.nagarajan95@gmail.com>
2016-10-23 14:39:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,8 +8,8 @@ element.innerHTML='new value';
 
 var img=document.getElementById('madi');
 var marginLeft=0;
-function marginRight() {
-    marginLeft=marginLeft+ 10;
+function moveRight() {
+    marginLeft=marginLeft + 10;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
