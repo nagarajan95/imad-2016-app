1afcb7be322d186318709992a069416d775d6422
nagarajan95 <l.nagarajan95@gmail.com>
2016-10-23 15:21:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,10 @@
-console.log('Loaded!');
+//counter
+var button=document.getElementById('counter');
+var counter=0;
 
-//change the text of the main-text div
-var element = document.getElementById('main-text');
-element.innerHTML='new value';
 
-//move image
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight() {
-    marginLeft=marginLeft + 5;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval = setInterval(moveRight,100)
+button.onclick=function() {
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
